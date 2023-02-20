8b9700c7f7b08f94e23e96b8a6ddda93640c345e
Giorgio Vilardo <giorgio@vilardo.eu>
2023-02-20 21:33:54 +0100

    refactor: token repository tests

--- token_repository_test.go
+++ token_repository_test.go
@@ -7,75 +7,50 @@ import (
 	"time"
 )
 
-func TestNewHttpTokenRepository(t *testing.T) {
+func TestHttpTokenRepository_GetToken(t *testing.T) {
 	clock := NewBrokenClock(time.Date(2021, 8, 25, 8, 30, 0, 0, time.UTC))
-	got := NewHttpTokenRepository(clock, mockSoundcloudApi{})
-	assert.False(t, got.initialized)
-}
-
-func TestHttpTokenRepository_GetTokenOnFirstRunInitializes(t *testing.T) {
-	clock := NewBrokenClock(time.Date(2021, 8, 25, 8, 30, 0, 0, time.UTC))
-	got := NewHttpTokenRepository(clock, mockSoundcloudApi{})
-	_, _ = got.GetToken()
-	assert.True(t, got.initialized)
-}
-
-func TestHttpTokenRepository_GetTokenFirstRun(t *testing.T) {
-	clock := NewBrokenClock(time.Date(2021, 8, 25, 8, 30, 0, 0, time.UTC))
-	got, _ := NewHttpTokenRepository(clock, mockSoundcloudApi{}).GetToken()
-	assert.Equal(t, "miao", got.AccessToken)
-	assert.Equal(t, "bau", got.RefreshToken)
-	assert.Equal(t, clock.Now().Add(time.Second*time.Duration(got.ExpiresIn)), got.ExpiresAt)
-}
-
-func TestHttpTokenRepository_GetTokenRenew(t *testing.T) {
-	clock := NewBrokenClock(time.Date(2021, 8, 25, 8, 30, 0, 0, time.UTC))
-	repository := HttpTokenRepository{
-		currentToken: Token{ExpiresAt: time.Date(1999, 1, 1, 1, 1, 1, 1, time.UTC)},
-		sc:           mockSoundcloudApi{},
-		initialized:  true,
-		clock:        clock,
+	repoWith := func(api SoundcloudApi) *HttpTokenRepository {
+		return &HttpTokenRepository{
+			currentToken: Token{ExpiresAt: time.Date(1999, 1, 1, 1, 1, 1, 1, time.UTC)},
+			sc:           api,
+			initialized:  true,
+			clock:        clock,
+		}
 	}
-	got, _ := repository.GetToken()
-	assert.Equal(t, "miao_renewed", got.AccessToken)
-	assert.Equal(t, "bau", got.RefreshToken)
-	assert.Equal(t, clock.Now().Add(time.Second*time.Duration(got.ExpiresIn)), got.ExpiresAt)
-}
 
-func TestHttpTokenRepository_GetTokenAuthFailsError(t *testing.T) {
-	clock := NewBrokenClock(time.Date(2021, 8, 25, 8, 30, 0, 0, time.UTC))
-	_, err := NewHttpTokenRepository(clock, mockFailingSoundcloudApi{}).GetToken()
-	assert.Equal(t, "auth_fail", err.Error())
-}
+	t.Run("should return a token", func(t *testing.T) {
+		got, _ := NewHttpTokenRepository(clock, mockSoundcloudApi{}).GetToken()
+		assert.Equal(t, "miao", got.AccessToken)
+		assert.Equal(t, "bau", got.RefreshToken)
+		assert.Equal(t, clock.Now().Add(time.Second*time.Duration(got.ExpiresIn)), got.ExpiresAt)
+	})
 
-func TestHttpTokenRepository_GetTokenRenewFailsError(t *testing.T) {
-	clock := NewBrokenClock(time.Date(2021, 8, 25, 8, 30, 0, 0, time.UTC))
-	repository := HttpTokenRepository{
-		currentToken: Token{ExpiresAt: time.Date(1999, 1, 1, 1, 1, 1, 1, time.UTC)},
-		sc:           mockFailingSoundcloudApi{},
-		initialized:  true,
-		clock:        clock,
-	}
-	_, err := repository.GetToken()
-	assert.Equal(t, "renew_fail", err.Error())
-}
+	t.Run("should transparently renew the token if expired", func(t *testing.T) {
+		got, _ := repoWith(mockSoundcloudApi{}).GetToken()
+		assert.Equal(t, "miao_renewed", got.AccessToken)
+		assert.Equal(t, "bau", got.RefreshToken)
+		assert.Equal(t, clock.Now().Add(time.Second*time.Duration(got.ExpiresIn)), got.ExpiresAt)
+	})
 
-func TestHttpTokenRepository_GetTokenAuthJsonFailsError(t *testing.T) {
-	clock := NewBrokenClock(time.Date(2021, 8, 25, 8, 30, 0, 0, time.UTC))
-	_, err := NewHttpTokenRepository(clock, mockJsonFailingSoundcloudApi{}).GetToken()
-	assert.Equal(t, "json: cannot unmarshal number into Go value of type main.Token", err.Error())
-}
+	t.Run("should return the same error from the api if auth fails", func(t *testing.T) {
+		_, err := NewHttpTokenRepository(clock, mockFailingSoundcloudApi{}).GetToken()
+		assert.Equal(t, "auth_fail", err.Error())
+	})
 
-func TestHttpTokenRepository_GetTokenRenewJsonFailsError(t *testing.T) {
-	clock := NewBrokenClock(time.Date(2021, 8, 25, 8, 30, 0, 0, time.UTC))
-	repository := HttpTokenRepository{
-		currentToken: Token{ExpiresAt: time.Date(1999, 1, 1, 1, 1, 1, 1, time.UTC)},
-		sc:           mockJsonFailingSoundcloudApi{},
-		initialized:  true,
-		clock:        clock,
-	}
-	_, err := repository.GetToken()
-	assert.Equal(t, "json: cannot unmarshal number into Go value of type main.Token", err.Error())
+	t.Run("should return the same error from the api if renew fails", func(t *testing.T) {
+		_, err := repoWith(mockFailingSoundcloudApi{}).GetToken()
+		assert.Equal(t, "renew_fail", err.Error())
+	})
+
+	t.Run("should fail if auth can't deserialize json into token", func(t *testing.T) {
+		_, err := NewHttpTokenRepository(clock, mockJsonFailingSoundcloudApi{}).GetToken()
+		assert.Equal(t, "json: cannot unmarshal number into Go value of type main.Token", err.Error())
+	})
+
+	t.Run("should fail if renew can't deserialize json into token", func(t *testing.T) {
+		_, err := repoWith(mockJsonFailingSoundcloudApi{}).GetToken()
+		assert.Equal(t, "json: cannot unmarshal number into Go value of type main.Token", err.Error())
+	})
 }
 
 type mockSoundcloudApi struct{}
